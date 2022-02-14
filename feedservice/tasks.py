f1cb98e3e94dbe613ded998fe626fa3082cc5f04
Jesse <j3bsie@gmail.com>
2022-02-14 04:13:48 +0100

    Fix: background task update - Add better logging - Remove returns to allow better interation

--- feedservice/tasks.py
+++ feedservice/tasks.py
@@ -1,3 +1,5 @@
+import logging
+
 from celery import shared_task
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -6,6 +8,8 @@ from utils.helpers import EmailManager, FeedManager
 
 import feedservice.service as fs
 from feedservice.models import Feed, FeedItems
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def notify_users(self, exc, task_id, args, kwargs, einfo):
@@ -29,16 +33,18 @@ def notify_users(self, exc, task_id, args, kwargs, einfo):
     on_failure=notify_users,
 )
 def update_feed_and_items(self, feed_id: int = None):
-    """Background Task to Update Feed and Feed Items"""
+    """Background task to update single or multiple feeds"""
+
     if feed_id:
         feed_qs = Feed.objects.filter(id=feed_id)
         if not len(feed_qs):
-            return _("Feed not found")
+            return "Feed not found"
     else:
         feed_qs = Feed.objects.all()
 
-    # Fetch records in an efficient and iterative manner.
+    # Fetch records in chuncks.
     for feed in feed_qs.iterator(chunk_size=1 if not feed_id else 1000):
+        logging.info(f"Fetching feed data from {feed.url}.")
         feed_data = FeedManager.parse_feed_url(feed.url)
         feed_resource_last_update = FeedManager.parse_feed_time(feed_data.updated)
 
@@ -49,17 +55,20 @@ def update_feed_and_items(self, feed_id: int = None):
             feed.updated_at = feed_resource_last_update
             feed.save(update_fields=["title", "updated_at"])
 
-        all_feed_item = list(FeedItems.objects.filter(feed=feed).values("entry_id", "published_at"))
-        if len(all_feed_item) < 1:
+        feed_items = list(FeedItems.objects.filter(feed=feed).values("entry_id", "published_at"))
+
+        if len(feed_items) < 1:
+            # This is for a case where the feed didn't have items when it was first added, we update here.
             created_feed_items = fs.FeedService.create_feed_items_from_entries(
                 entries=feed_data.entries, feed_id=feed.id
             )
-            return _(f"Added {len(created_feed_items)} feed items.")
+            logging.info(f"Added {len(created_feed_items)} new feed items from {feed.url}.")
+            continue
 
-        most_recent_feed_item = all_feed_item[0]
-        all_entry_ids = [entry_id["entry_id"] for entry_id in all_feed_item]
+        most_recent_feed_item = feed_items[0]
+        all_entry_ids = [entry_id["entry_id"] for entry_id in feed_items]
 
-        # We check that only items that has not been added before are persisted
+        # We check that only items that has not been added before are added
         filtered_entried = [
             item_data
             for item_data in feed_data.entries
@@ -69,4 +78,6 @@ def update_feed_and_items(self, feed_id: int = None):
 
         created_feed_items = fs.FeedService.create_feed_items_from_entries(entries=filtered_entried, feed_id=feed.id)
 
-        return _(f"Added {len(created_feed_items)} feed items.")
+        logging.info(f"Added {len(created_feed_items)} new feed items from {feed.url}.")
+
+    logging.info(f"Feed update completed, We're up to speed now! Phewwww!!!")
